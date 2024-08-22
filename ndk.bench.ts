b971b56182db344975a04bb06f1ee70da5ed0c96
SnowCait <SnowCait@users.noreply.github.com>
2024-08-22 15:09:16 +0900

    Benchmarks

---
+++ ndk.bench.ts
@@ -0,0 +1,37 @@
+import NDK, { NDKEvent, NDKPrivateKeySigner } from "npm:@nostr-dev-kit/ndk";
+import { generateSecretKey } from "npm:nostr-tools";
+
+Deno.bench("sign", async (b) => {
+  const seckey = generateSecretKey();
+  const signer = new NDKPrivateKeySigner(seckey);
+  const ndk = new NDK({ signer });
+
+  b.start();
+
+  const event = new NDKEvent(ndk);
+  event.kind = 1;
+  event.content = "";
+  event.tags = [];
+  event.created_at = 0;
+  await event.sign(); // Internally, call finalizeEvent of nostr-tools
+
+  b.end();
+});
+
+Deno.bench("isValid", async (b) => {
+  const seckey = generateSecretKey();
+  const signer = new NDKPrivateKeySigner(seckey);
+  const ndk = new NDK({ signer });
+  const event = new NDKEvent(ndk);
+  event.kind = 1;
+  event.content = "";
+  event.tags = [];
+  event.created_at = 0;
+  await event.sign();
+
+  b.start();
+
+  event.isValid;
+
+  b.end();
+});
