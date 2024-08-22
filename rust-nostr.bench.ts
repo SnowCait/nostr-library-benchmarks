8a5545cae9aca63c110fdcdebb70168d171e4536
SnowCait <SnowCait@users.noreply.github.com>
2024-08-22 16:34:58 +0900

    Benchmarks

---
+++ rust-nostr.bench.ts
@@ -0,0 +1,34 @@
+import { EventBuilder, Keys, loadWasmAsync } from "npm:@rust-nostr/nostr";
+
+Deno.bench("generate", async (b) => {
+    await loadWasmAsync();
+
+    b.start();
+
+    Keys.generate();
+
+    b.end();
+});
+
+Deno.bench("toEvent", async (b) => {
+    await loadWasmAsync();
+    const keys = Keys.generate();
+
+    b.start();
+
+    new EventBuilder(1, "", []).toEvent(keys).asJson();
+
+    b.end();
+});
+
+Deno.bench("verify", async (b) => {
+    await loadWasmAsync();
+    const keys = Keys.generate();
+    const event = new EventBuilder(1, "", []).toEvent(keys);
+
+    b.start();
+
+    event.verify();
+
+    b.end();
+});
